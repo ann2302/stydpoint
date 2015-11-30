777abb892864a5eca6224c0dbc072c1cc3c0c3f7
ann2302 <annuk2302@mail.ru>
2015-12-01 01:28:53 +0200

    Four commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
 
-        var $menu = $("#menu");
+	var $menu = $("#menu");
 
-        $(window).scroll(function(){
-            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
-                $menu.removeClass("default").addClass("fixed");
-            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
-                $menu.removeClass("fixed").addClass("default");
-            }
-        });//scroll
-    });
+	$(window).scroll(function(){
+		if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+			$menu.removeClass("default").addClass("fixed");
+			$("#log").css({visibility:"visible"});
+			$("#log").addClass('animated fadeInLeft');
+		}
+		else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+			$("#log").css({visibility:"hidden"});
+			$menu.removeClass("fixed").addClass("default");
+			
+		}
+	});
+});
+        //scroll
+
+
+
+
+
+
+
+
+
